c0b7cb28ac2264c60ff50c235bc7956a407969cb
ang_zy <oej3659@daum.net>
2021-05-30 03:08:49 +0900

    change server.js

--- server/server.js
+++ server/server.js
@@ -5,14 +5,19 @@ const config = require('./config/key');
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 
-const mongoose = require('mongoose')
-mongoose.connect(config.mongoURI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true
-}).then(() => console.log('MongoDB connected...'))
-    .catch(err => console.log(err))
+const mongoose = require("mongoose");
+
+(async () => {
+    try {
+        await mongoose.connect(config.mongoURI, {
+            useNewUrlParser: true, useUnifiedTopology: true,
+            useCreateIndex: true, useFindAndModify: false, poolSize: 100
+        });
+        console.log('MongoDB Connected...');
+    } catch(err) {
+        console.log(err)
+    }
+})()
 
 //application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended: true}));
@@ -27,8 +32,24 @@ app.use('/api/video', require('./routes/video'));
 app.use('/api/subscribe', require('./routes/subscribe'));
 app.use('/api/like', require('./routes/like'));
 
+if (process.env.NODE_ENV === "production") {
 
-const port = 5000;
+    // Set static folder   
+    // All the javascript and css files will be read and served from this folder
+    app.use(express.static("client/build"));
+
+    // index.html for all page routes    html or routing and naviagtion
+    // app.get("*", (req, res) => {
+    //   res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
+    // });
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname+'/client/build/index.html'));
+    });
+}
+
+const port = process.env.PORT || 5000
+
 app.listen(port, () => {
     console.log(`Server Listening on ${port}`)
 })
