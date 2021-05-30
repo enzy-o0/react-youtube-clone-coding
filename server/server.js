43f32e4f0db5bb08fe50fa92262f347a5544e0f9
ang_zy <oej3659@daum.net>
2021-05-30 15:20:54 +0900

    server.js 수정

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const config = require('./config/key');
+const path = require("path");
 
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
